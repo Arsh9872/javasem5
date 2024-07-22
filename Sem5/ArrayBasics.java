9e43eb961dc536228357a529b843d73318129c0a
Arsh9872 <arshpreetsingh0089@gmail.com>
2024-07-22 20:06:06 +0530

    Array 1d Basics

---
+++ Sem5/ArrayBasics.java
@@ -0,0 +1,41 @@
+package Sem5;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayBasics {
+    public static void main(String[] args) {
+        //basics
+    //     int[] numbers = {1,2,3,4,5}; // array at complie time
+
+    //     System.out.print("array = ");
+    //     for (int i = 0; i < numbers.length; i++) {
+    //         System.out.print(numbers[i]+" ");
+    // }
+
+
+    Scanner sc= new Scanner(System.in);
+    System.out.print("Enter number of variables: ");
+    int n = sc.nextInt();
+    System.out.print("Enter variables: ");
+    int [] arr = new int[n];      //array usage time
+    // int[] arr = new sc.nextInt();  // anonymous variable type input
+    for (int i = 0; i < arr.length; i++) {
+        arr[i] = sc.nextInt();
+    }  
+    // for (int i = 0; i < arr.length; i++) {
+    //     System.out.print(arr[i]+" ");
+    // }
+    // System.out.println(" ");
+    // for (int j : arr) {    // foreach method
+    //     System.out.print(arr[j]+" ");
+    // }
+
+    System.out.println(Arrays.toString(arr));
+    
+
+        }
+
+        
+    
+}
