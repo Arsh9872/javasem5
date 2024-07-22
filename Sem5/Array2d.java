ae5556aed12a3d427db18182021f06fad801a782
Arsh9872 <arshpreetsingh0089@gmail.com>
2024-07-22 20:06:15 +0530

    2d array

---
+++ Sem5/Array2d.java
@@ -0,0 +1,21 @@
+package Sem5;
+
+public class Array2d {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[i][j] = i*j;
+            }
+            
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j]+" ");
+            }
+            
+        }
+    }
+    
+}
