63f1021bad7db449e70ea732eb133018da695dbc
Arsh9872 <arshpreetsingh0089@gmail.com>
2024-07-23 19:40:04 +0530

    string basics

---
+++ Sem5/string.java
@@ -0,0 +1,13 @@
+package Sem5;
+
+public class string {
+    public static void main(String[] args) {
+        // function that belong to a class is called method otherwise its a function only
+        // sort(array,fromindex,toindex)  =  sort from (fromindex) to (toindex -1)
+        //int[] num = [4,3,5,6,3,1,4,4]
+        // Arrays.sort(num,3,7)       //output = 5,4,3,-1,0,0,1,2
+        // Point[] points = {new Point(1,2), new Point(3,4), new Point(-1,-2)};
+        // Arrays.sort(points)                // throw error as it sort and compare numbers but not references 
+    }
+    
+}
