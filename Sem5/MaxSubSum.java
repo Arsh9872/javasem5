a0d12e32fb871e94dfcdba51cf77cb7da061e6aa
Arsh9872 <arshpreetsingh0089@gmail.com>
2024-07-25 20:11:27 +0530

    Printing 2d Array

--- Sem5/MaxSubSum.java
+++ Sem5/MaxSubSum.java
@@ -1,7 +1,14 @@
 package Sem5;
 
+import java.util.Arrays;
+
 public class MaxSubSum {
     public static void main(String[] args) {
-        
+
+     int[][] arr= {{1,2,3},{2,4,6},{3,7,9}};
+
+    //  System.out.println(arr); // if print directly it will print only address
+    System.out.println(Arrays.toString(arr));  //print address because array is inside array
+
     }
 }
