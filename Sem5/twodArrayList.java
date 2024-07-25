d363edb6f11e68fa28b6da5c059671e59b9e946b
Arsh9872 <arshpreetsingh0089@gmail.com>
2024-07-25 20:43:52 +0530

    Array list basics - add elements

---
+++ Sem5/twodArrayList.java
@@ -0,0 +1,19 @@
+package Sem5;
+// import java.util.ArrayList;
+import java.util.*;
+
+public class twodArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>() ;
+        System.out.println(arr);
+        arr.add(12);
+        arr.add(22);
+        arr.add(32);
+        arr.add(42);
+        arr.add(52);
+        System.out.println(arr);
+        arr.add(1,62);
+        System.out.println(arr);
+    }
+    
+}
