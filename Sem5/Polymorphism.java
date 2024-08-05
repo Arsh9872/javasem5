e75dbbdafc3bb7f20fffb16e84c223fcd1b77254
Arsh9872 <arshpreetsingh0089@gmail.com>
2024-08-05 19:42:21 +0530

    concept of character addition

--- Sem5/Polymorphism.java
+++ Sem5/Polymorphism.java
@@ -4,10 +4,14 @@ public class Polymorphism {
     public static void main(String[] args) {
 
         // basic example of operator overloading
-        
+
         // int a= 10;
-        // int b= 10;
+        // int b= 20;
         // System.out.println(a+b);
+        // System.out.println("Sum of Numbers is :"+ a+b);
+
+        // System.out.println('a' + 'b');   // these are characters so it add ascii values if in""" it will concatenate
+
     }
     
 }
