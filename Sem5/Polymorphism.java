cdd2b89117dddfae1fcac7124683cd383b0520b4
Arsh9872 <arshpreetsingh0089@gmail.com>
2024-08-05 19:52:33 +0530

    problem of operator overloading in all aspects

--- Sem5/Polymorphism.java
+++ Sem5/Polymorphism.java
@@ -11,7 +11,14 @@ public class Polymorphism {
         // System.out.println("Sum of Numbers is :"+ a+b);
 
         // System.out.println('a' + 'b');   // these are characters so it add ascii values if in""" it will concatenate
-        System.out.println("a" + 'b');
+
+        // System.out.println("a" + 'b');       // if there is a string before + then it will auto take other as string
+
+        // System.out.println('a' + "b");   // but string is a string it will give output as concat of ab
+
+        System.out.println('a' + 'b' + "c");   // 195c
+
+
 
     }
     
