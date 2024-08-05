e13ebfaab9696ae39ba013001a5c811ee5700573
Arsh9872 <arshpreetsingh0089@gmail.com>
2024-08-05 19:36:34 +0530

    Polymorphism - classic example of operator overloadnig

---
+++ Sem5/Polymorphism.java
@@ -0,0 +1,13 @@
+package Sem5;
+
+public class Polymorphism {
+    public static void main(String[] args) {
+
+        // basic example of operator overloading
+        
+        // int a= 10;
+        // int b= 10;
+        // System.out.println(a+b);
+    }
+    
+}
