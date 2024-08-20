b2c85aa493e08addc363b0383b2b8c9080d8a4ab
Arsh9872 <arshpreetsingh0089@gmail.com>
2024-08-20 20:00:52 +0530

    cont.

---
+++ Sem5/ShellRotate.java
@@ -0,0 +1,15 @@
+package Sem5;
+
+import java.util.Scanner;
+
+public class ShellRotate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m =sc.nextInt();
+        int [][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            
+        }
+    }
+}
